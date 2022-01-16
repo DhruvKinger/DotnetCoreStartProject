303c0b9d6cab18e5d24de27d1d682693f7b8fc9c
Dhruv Kinger <cse1785@gmail.com>
2022-01-16 22:57:20 +0530

    Domain Classes Added ...

---
+++ DomainProject/Entities/Category.cs
@@ -0,0 +1,13 @@
+﻿using DomainProject.Common;
+using System;
+using System.Collections.Generic;
+
+namespace DomainProject.Entities
+{
+    public class Category: AuditableEntity
+    {
+        public Guid CategoryId { get; set; }
+        public string Name { get; set; }
+        public ICollection<Event> Events { get; set; }
+    }
+}
