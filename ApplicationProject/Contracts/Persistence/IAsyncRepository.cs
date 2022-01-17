5d96a0081b93377ace10a0176e351dc0d40c5c5d
Dhruv Kinger <cse1785@gmail.com>
2022-01-17 21:53:55 +0530

    Repository's Set up in Project !!

---
+++ ApplicationProject/Contracts/Persistence/IAsyncRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationProject.Contracts.Persistence
+{
+    public interface IAsyncRepository<T> where T: class
+    {
+        Task<T> GetIdByAsync(Guid Id);
+        Task<T> AddAsync(T entity);
+        Task<T> UpdateAsync(T entity); 
+        Task<T> DeleteAsync(T entity);
+        Task<IReadOnlyList<T>> ListAllAsync(); 
+
+    }
+}
---
+++ ApplicationProject/Contracts/Persistence/IOrderRepository.cs
@@ -0,0 +1,13 @@
+﻿using DotnetCoreStartProject.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationProject.Contracts.Persistence
+{
+    public interface IOrderRepository: IAsyncRepository<Order>
+    {
+       
+    }
+}
